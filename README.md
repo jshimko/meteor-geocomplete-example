8dab857b68fa77fbcc76c42ebd46b02d59ec3dcd
Jeremy Shimko <jeremy.shimko@gmail.com>
2015-03-05 23:10:42 -0500

    Init()

---
+++ README.md
@@ -0,0 +1,18 @@
+# Geocomplete Example
+
+This is example usage of the jeremy:geocomplete package for Meteor.
+
+Demo: http://geocomplete.meteor.com
+
+## Install
+
+```
+meteor install jeremy:geocomplete
+```
+
+
+## More Info
+
+[Atmosphere](https://atmospherejs.com/jeremy/geocomplete)
+
+[Github](https://github.com/jshimko/meteor-geocomplete/)
---
+++ meteor-geocomplete-example.js
@@ -0,0 +1,37 @@
+if (Meteor.isClient) {
+
+  // Load the Google Maps API on startup
+  Meteor.startup(function() {
+    GoogleMaps.load({
+      key: 'YOUR API KEY',
+      libraries: 'places'
+    });
+  });
+
+
+  Template.geocompleteExample.rendered = function () {
+
+    Tracker.autorun(function () {
+      // Wait for API to be loaded
+      if (GoogleMaps.loaded()) {
+
+        // Example 1 - Autocomplete only
+        $('#place1').geocomplete();
+
+        // Example 2 - Autocomplete + map
+        $('#place2').geocomplete({
+          map: $("#map")
+        });
+
+        // Example 3 - Autocomplete + map + form
+        $('#place3').geocomplete({
+          map: "#map2",
+          details: "form"
+        });
+
+      }
+    });
+
+  }
+
+}
